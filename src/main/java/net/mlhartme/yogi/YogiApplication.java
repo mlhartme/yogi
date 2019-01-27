457e789f47d6fc5e97f743be99cdd9cd7f694af2
Michael Hartmeier <mlhartme@github.com>
2019-01-27 22:32:55 +0100

    newly generated

---
+++ src/main/java/net/mlhartme/yogi/YogiApplication.java
@@ -0,0 +1,14 @@
+package net.mlhartme.yogi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogiApplication.class, args);
+	}
+
+}
+
