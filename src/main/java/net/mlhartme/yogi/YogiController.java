469c5a5bfea536df267a47c326ce57376ace5476
Michael Hartmeier <mlhartme@github.com>
2019-01-29 22:35:22 +0100

    list variables

--- src/main/java/net/mlhartme/yogi/YogiController.java
+++ src/main/java/net/mlhartme/yogi/YogiController.java
@@ -1,13 +1,19 @@
 package net.mlhartme.yogi;
 
+import net.oneandone.sushi.fs.Node;
 import net.oneandone.sushi.fs.World;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class YogiController {
@@ -17,12 +23,36 @@ public class YogiController {
         this.world = world;
     }
 
-    @GetMapping({"/", "/{name}"})
-    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="test") String name) throws IOException {
-        List<String> lines;
+    @RequestMapping("/{file}")
+    public String hello(Model model, @PathVariable("file") String file) throws IOException {
+        Map<String, String> lines;
 
-        lines = world.resource("BOOT-INF/classes/data/english/test.yogi").readLines();
+        lines = load(world.resource("BOOT-INF/classes/data/english/test.yogi"));
         model.addAttribute("lines", lines);
         return "yogi";
     }
+
+    private static Map<String, String> load(Node<?>... files) throws IOException {
+        List<String> lines;
+        Map<String, String> map;
+        int idx;
+
+        lines = new ArrayList<>();
+        for (Node<?> file : files) {
+            lines.addAll(file.readLines());
+        }
+        map = new LinkedHashMap<>();
+        for (String line : lines) {
+            if (line.trim().isEmpty()) {
+                continue;
+            }
+            idx = line.indexOf('=');
+            if (idx == -1) {
+                throw new IOException("syntax error: " + line);
+            }
+            map.put(line.substring(0, idx).trim(), line.substring(idx + 1).trim());
+        }
+        return map;
+    }
+
 }
