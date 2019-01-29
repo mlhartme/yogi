41626a43bde30b1c9931672bed384a9d0344df23
Michael Hartmeier <mlhartme@github.com>
2019-01-29 21:57:40 +0100

    show file

--- src/main/java/net/mlhartme/yogi/YogiApplication.java
+++ src/main/java/net/mlhartme/yogi/YogiApplication.java
@@ -1,14 +1,21 @@
 package net.mlhartme.yogi;
 
+import net.oneandone.sushi.fs.World;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class YogiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(YogiApplication.class, args);
 	}
 
+	@Bean
+	public World world() throws IOException {
+		return World.create();
+	}
 }
 
--- src/main/java/net/mlhartme/yogi/YogiController.java
+++ src/main/java/net/mlhartme/yogi/YogiController.java
@@ -1,15 +1,28 @@
 package net.mlhartme.yogi;
 
+import net.oneandone.sushi.fs.World;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
+import java.util.List;
+
 @Controller
 public class YogiController {
-    @GetMapping({"/", "/hello"})
-    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
-        model.addAttribute("name", name);
+    private World world;
+
+    public YogiController(World world) {
+        this.world = world;
+    }
+
+    @GetMapping({"/", "/{name}"})
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="test") String name) throws IOException {
+        List<String> lines;
+
+        lines = world.resource("BOOT-INF/classes/data/english/test.yogi").readLines();
+        model.addAttribute("lines", lines);
         return "yogi";
     }
 }
