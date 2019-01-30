efdde0fc3bf652c8a654b76cc9ceb804f747b683
Michael Hartmeier <mlhartme@github.com>
2019-01-30 22:41:35 +0100

    cleanup

--- src/main/java/net/mlhartme/yogi/YogiController.java
+++ src/main/java/net/mlhartme/yogi/YogiController.java
@@ -1,20 +1,13 @@
 package net.mlhartme.yogi;
 
-import net.oneandone.sushi.fs.Node;
 import net.oneandone.sushi.fs.World;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 @Controller
