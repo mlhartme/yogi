715426774838bbf241452a18261b23af6ed0e389
Michael Hartmeier <mlhartme@github.com>
2019-04-09 23:28:29 +0200

    done param

--- src/main/java/net/mlhartme/yogi/YogiController.java
+++ src/main/java/net/mlhartme/yogi/YogiController.java
@@ -38,22 +38,45 @@ public class YogiController {
         return "index";
     }
     @RequestMapping("/{unit}")
-    public String question(Model model, @PathVariable("unit") String unit, @RequestParam(value = "done", required = false) String doneParam) throws IOException {
+    public String question(Model model, @PathVariable("unit") String unit,
+                           @RequestParam(value = "idx", required = false) Integer idxParam,
+                           @RequestParam(value = "done", required = false) String doneParam) throws IOException {
         Vocabulary vocabulary;
         int idx;
         String word;
         List<Integer> done;
 
-        done = toInt(doneParam == null ? new ArrayList<>() : Separator.COMMA.split(doneParam));
+        done = doneParam(doneParam);
         vocabulary = Vocabulary.load(base.join(unit + ".txt"));
-        idx = vocabulary.next(done);
+        if (idxParam == null) {
+            idx = vocabulary.next(done);
+        } else {
+            idx = idxParam;
+        }
         word = vocabulary.left(idx);
         model.addAttribute(word);
         model.addAttribute("idx", idx);
         model.addAttribute("word", word);
         model.addAttribute("unit", unit);
-        model.addAttribute("done", done);
+        model.addAttribute("done", toString(done));
         return "question";
+    }
+
+    private static List<Integer> doneParam(String doneParam) {
+        return toInt(doneParam == null ? new ArrayList<>() : Separator.COMMA.split(doneParam));
+    }
+
+    private static String toString(List<Integer> done) {
+        StringBuilder result;
+
+        result = new StringBuilder();
+        for (Integer i : done) {
+            if (result.length() > 0) {
+                result.append(',');
+            }
+            result.append(i);
+        }
+        return result.toString();
     }
 
     private static List<Integer> toInt(List<String> strings) {
@@ -66,16 +89,25 @@ public class YogiController {
         return result;
     }
 
-    @RequestMapping("/{file}/answer.html")
-    public String answer(Model model, @PathVariable("file") String file, @RequestParam("answer") String answer,
-                         @RequestParam("idx") int idx) throws IOException {
+    @RequestMapping("/{unit}/answer.html")
+    public String answer(Model model, @PathVariable("unit") String unit, @RequestParam("answer") String answer,
+                         @RequestParam("idx") int idx, @RequestParam("done") String doneParam) throws IOException {
         Vocabulary vocabulary;
+        List<Integer> done;
+        String expected;
+        boolean correct;
 
-        file = file.replace("=", "/") + ".txt";
-        vocabulary = Vocabulary.load(base.join(file));
-
+        done = doneParam(doneParam);
+        vocabulary = Vocabulary.load(base.join(unit + ".txt"));
+        expected = vocabulary.right(idx);
+        correct = answer.equals(expected);
+        if (correct) {
+            done.add(idx);
+        }
         model.addAttribute("answer", answer);
-        model.addAttribute("correct", vocabulary.right(idx));
+        model.addAttribute("expected", expected);
+        model.addAttribute("correct", correct);
+        model.addAttribute("done", toString(done));
         return "answer";
     }
 }
