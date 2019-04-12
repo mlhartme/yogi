aef516ce968a7dc37412b59d635836ca94fbdbde
Michael Hartmeier <mlhartme@github.com>
2019-04-12 22:54:53 +0200

    rounds

--- src/main/java/net/mlhartme/yogi/Exercise.java
+++ src/main/java/net/mlhartme/yogi/Exercise.java
@@ -8,37 +8,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Exercise {
+    private static final Separator COLON = Separator.on(':');
+
     public static Exercise forParam(Node<?> base, String param) throws IOException {
-        int idx;
+        List<String> args;
+
+        args = COLON.split(param);
+
         String unit;
+        int round;
         String ok;
         String wrong;
 
-        idx = param.indexOf(':');
-        if (idx == -1) {
-            unit = param;
-            ok = null;
-            wrong = null;
+        unit = eat(args, "");
+        round = Integer.parseInt(eat(args, "1"));
+        ok = eat(args, null);
+        wrong = eat(args, null);
+        return create(base, unit, round, ok, wrong);
+    }
+
+    private static String eat(List<String> lst, String dflt) {
+        if (lst.isEmpty()) {
+            return dflt;
         } else {
-            unit = param.substring(0, idx);
-            param = param.substring(idx + 1);
-            idx = param.indexOf(':');
-            if (idx == -1) {
-                ok = param;
-                wrong = null;
-            } else {
-                ok = param.substring(0, idx);
-                wrong = param.substring(idx + 1);
-            }
+            return lst.remove(0);
         }
-        return create(base, unit, ok, wrong);
     }
 
     public static Exercise create(Node<?> base, String unit) throws IOException {
-        return create(base, unit, null, null);
+        return create(base, unit, 1, null, null);
     }
 
-    public static Exercise create(Node<?> base, String unit, String okParam, String wrongParam) throws IOException {
+    public static Exercise create(Node<?> base, String unit, int round, String okParam, String wrongParam) throws IOException {
         List<Integer> ok;
         List<Integer> wrong;
         Vocabulary vocabulary;
@@ -46,23 +47,37 @@ public class Exercise {
         vocabulary = Vocabulary.load(base.join(unit + ".txt"));
         ok = toInt(okParam == null ? new ArrayList<>() : Separator.COMMA.split(okParam));
         wrong = toInt(wrongParam == null ? new ArrayList<>() : Separator.COMMA.split(wrongParam));
-        return new Exercise(unit, vocabulary, ok, wrong);
+        return new Exercise(unit, vocabulary, round, ok, wrong);
     }
 
     public final String unit;
     public final Vocabulary vocabulary;
+    public int round;
     public final List<Integer> ok;
     public final List<Integer> wrong;
 
-    public Exercise(String unit, Vocabulary vocabulary, List<Integer> ok, List<Integer> wrong) {
+    public Exercise(String unit, Vocabulary vocabulary, int round, List<Integer> ok, List<Integer> wrong) {
+        if (vocabulary.size() == 0) {
+            throw new IllegalArgumentException();
+        }
         this.unit = unit;
         this.vocabulary = vocabulary;
+        this.round = round;
         this.ok = ok;
         this.wrong = wrong;
     }
 
     public String question() {
-        return vocabulary.left(vocabulary.next(ok));
+        if (ok.size() + wrong.size() == vocabulary.size()) {
+            round++;
+            if (wrong.isEmpty()) {
+                ok.clear();
+                // start from beginning
+            } else {
+                wrong.clear();
+            }
+        }
+        return vocabulary.left(vocabulary.next(union(ok, wrong)));
     }
 
     /** null if anwser is correct; otherwise the correct answer */
@@ -77,12 +92,15 @@ public class Exercise {
             ok.add(idx);
             return null;
         } else {
+            if (!wrong.contains(idx)) {
+                wrong.add(idx);
+            }
             return vocabulary.right(idx);
         }
     }
 
     public String toParam() {
-        return unit + ":" + toString(ok) + ":" + toString(wrong);
+        return unit + ":" + round + ":" + toString(ok) + ":" + toString(wrong);
     }
 
     public boolean allDone() {
@@ -91,6 +109,14 @@ public class Exercise {
 
     //--
 
+    private static List<Integer> union(List<Integer> left, List<Integer> right) {
+        List<Integer> result;
+
+        result = new ArrayList<>(left.size() + right.size());
+        result.addAll(left);
+        result.addAll(right);
+        return result;
+    }
 
     private static String toString(List<Integer> done) {
         StringBuilder result;
