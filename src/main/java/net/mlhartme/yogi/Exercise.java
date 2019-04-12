876775c72a7b1973222a1fd4341a8bb0731ed752
Michael Hartmeier <mlhartme@github.com>
2019-04-12 20:57:42 +0200

    exercise

--- src/main/java/net/mlhartme/yogi/Exercise.java
+++ src/main/java/net/mlhartme/yogi/Exercise.java
@@ -1,26 +1,36 @@
 package net.mlhartme.yogi;
 
+import net.oneandone.sushi.fs.Node;
 import net.oneandone.sushi.util.Separator;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Exercise {
-    public static Exercise forRequest(String doneParam) {
+    public static Exercise forRequest(Node<?> base, String unit, String doneParam) throws IOException {
         List<Integer> done;
+        Vocabulary vocabulary;
 
+        vocabulary = Vocabulary.load(base.join(unit + ".txt"));
         done = toInt(doneParam == null ? new ArrayList<>() : Separator.COMMA.split(doneParam));
-        return new Exercise(done);
+        return new Exercise(vocabulary, done);
     }
 
+    public final Vocabulary vocabulary;
     public final List<Integer> done;
 
-    public Exercise(List<Integer> done) {
+    public Exercise(Vocabulary vocabulary, List<Integer> done) {
+        this.vocabulary = vocabulary;
         this.done = done;
     }
 
     public String doneParam() {
         return toString(done);
+    }
+
+    public boolean allDone() {
+        return done.size() == vocabulary.size();
     }
 
     //--
--- src/main/java/net/mlhartme/yogi/YogiController.java
+++ src/main/java/net/mlhartme/yogi/YogiController.java
@@ -41,48 +41,45 @@ public class YogiController {
     public String question(Model model, @PathVariable("unit") String unit,
                            @RequestParam(value = "idx", required = false) Integer idxParam,
                            @RequestParam(value = "done", required = false) String doneParam) throws IOException {
-        Vocabulary vocabulary;
         int idx;
         String word;
         Exercise exercise;
 
-        exercise = Exercise.forRequest(doneParam);
-        vocabulary = Vocabulary.load(base.join(unit + ".txt"));
+        exercise = Exercise.forRequest(base, unit, doneParam);
         if (idxParam == null) {
-            idx = vocabulary.next(exercise.done);
+            idx = exercise.vocabulary.next(exercise.done);
         } else {
             idx = idxParam;
         }
-        word = vocabulary.left(idx);
+        word = exercise.vocabulary.left(idx);
         model.addAttribute(word);
+        model.addAttribute("exercise", exercise);
+
         model.addAttribute("idx", idx);
         model.addAttribute("word", word);
         model.addAttribute("unit", unit);
-        model.addAttribute("done", exercise.doneParam());
         return "question";
     }
 
     @RequestMapping("/{unit}/answer.html")
     public String answer(Model model, @PathVariable("unit") String unit, @RequestParam("answer") String answer,
                          @RequestParam("idx") int idx, @RequestParam("done") String doneParam) throws IOException {
-        Vocabulary vocabulary;
         Exercise exercise;
         String expected;
         boolean correct;
 
-        exercise = Exercise.forRequest(doneParam);
-        vocabulary = Vocabulary.load(base.join(unit + ".txt"));
-        expected = vocabulary.right(idx);
+        exercise = Exercise.forRequest(base, unit, doneParam);
+        expected = exercise.vocabulary.right(idx);
         correct = answer.equals(expected);
         if (correct) {
             exercise.done.add(idx);
         }
+        model.addAttribute("exercise", exercise);
+
         model.addAttribute("answer", answer);
         model.addAttribute("expected", expected);
         model.addAttribute("correct", correct);
-        model.addAttribute("allDone", exercise.done.size() == vocabulary.size());
         model.addAttribute("idx", idx);
-        model.addAttribute("done", exercise.doneParam());
         return "answer";
     }
 }
