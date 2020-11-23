926743600004575a05463f5a90c2f0bad3755107
Michael Hartmeier <mlhartme.users.noreply@github.com>
2020-11-23 08:45:04 +0100

    tweaks

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 echo "copying ..."
-docker save yogi:latest | bzip2 | ssh mops 'bunzip2 | docker load'
-ssh root@mops systemctl restart yogi
+docker save yogi:latest | bzip2 | ssh bachcraft.schmizzolin.de 'bunzip2 | docker load'
+ssh root@bachcraft.schmizzolin.de systemctl restart yogi
