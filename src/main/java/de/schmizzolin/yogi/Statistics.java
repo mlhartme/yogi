81ba2c6b62515928fa5cf72a3a14d45740733cda
Michael Hartmeier <mlhartme.users.noreply@github.com>
2021-10-30 19:27:33 +0200

    fix

--- src/main/java/de/schmizzolin/yogi/Statistics.java
+++ src/main/java/de/schmizzolin/yogi/Statistics.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- */
 package de.schmizzolin.yogi;
 
 import net.oneandone.sushi.fs.file.FileNode;
